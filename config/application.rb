06abcbe16d79ed648e613ae2ab940556b5a29a63
dbpatnodee <dbpatnode@hotmail.com>
2020-08-04 21:41:41 -0700

    Done.

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# class Application < Rails::Application
+#   config.web_console.whitelisted_ips = '<IP address here>'
+# end
+
 module BlogFlash
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
